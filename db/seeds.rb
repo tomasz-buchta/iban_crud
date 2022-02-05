c23248dea63469958328692127de9d56fcd9b535
Tomasz Buchta <tomasz.buchta@hotmail.com>
2022-02-05 19:39:39 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |i|
+  # FIXME: This can result in non unique iban number
+  iban = FactoryBot.build(:iban)
+  iban.name = "Iban acc no #{i}"
+  iban.save!
+end
 
