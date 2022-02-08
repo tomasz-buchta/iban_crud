3e23d7640efee8ee9ed0e7a33e6240e39e7ecb3f
Tomasz Buchta <tomasz.buchta@hotmail.com>
2022-02-08 22:29:45 +0100

    Add TODO for iban validation

--- app/models/iban.rb
+++ app/models/iban.rb
@@ -1,5 +1,6 @@
 class Iban < ApplicationRecord
   validates :name, presence: true
+  # TODO: There should proper iban validation - preventing user from saving malformed/invalid ibans
   validates :number, presence: true
 end
 
