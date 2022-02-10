28781e6c61073d84543e97aa9cd5ddc78a1dab95
Tomasz Buchta <tomasz.buchta@hotmail.com>
2022-02-10 12:59:50 +0100

    Add Iban number validation

--- app/models/iban.rb
+++ app/models/iban.rb
@@ -1,6 +1,30 @@
 class Iban < ApplicationRecord
   validates :name, presence: true
-  # TODO: There should proper iban validation - preventing user from saving malformed/invalid ibans
   validates :number, presence: true
+
+  validate :iban_checksum
+
+  EXTENDED_ASCII_SHIFT = 55
+
+  def iban_checksum
+    # TODO: This is only checking the checksum,
+    # proper iban validation is more complex than this, but it serves as good 1st step
+    # Next steps could be:
+    # - country code validation and
+    # - length validation(its country specific)
+    # - check BBAN
+    # - country specific iban format and structure
+    return unless number.present?
+
+    # Move first 4 charactes to the end of string
+    checked_number = number[4, number.length - 1] + number[0, 4]
+    # Replace letters with their respective digits as per ascii table (ex A => 10, B => 11)
+    checked_number =
+      checked_number
+      .chars
+      .map { |char| char.match(/[A-Z]/) ? char.ord - EXTENDED_ASCII_SHIFT : char }
+
+    errors.add(:number, "Checksum invalid") if checked_number.join.to_i % 97 != 1
+  end
 end
 
--- spec/models/iban_spec.rb
+++ spec/models/iban_spec.rb
@@ -3,5 +3,13 @@ require 'rails_helper'
 RSpec.describe Iban, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:number) }
+  it { is_expected.to allow_value("GB33BUKB20201555555555").for(:number) }
+  it "Iban with wrong checksum fails validation" do
+    is_expected.not_to allow_value("GB33BUKB20201555555544").for(:number)
+  end
+  it "Iban with unknown country code fails validation" do
+    skip("TODO: Implement iban number country code validation")
+    is_expected.not_to allow_value("XX33BUKB20201555555555").for(:number)
+  end
 end
 
